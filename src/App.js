2896e5b3103caeb0b1cd5507b7618bcf64a82676
Muhammad Abdullah <56094500+MuhammadAb98@users.noreply.github.com>
2020-12-09 23:39:47 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import React, {useEffect} from 'react';
+
+function App(){
+
+useEffect(()=>{
+  fetch('https://jsonplaceholder.typicode.com/posts/1')
+  .then((response) => response.json())
+  .then((json) => console.log(json))
+},[])
+
+return(
+
+  <div>
+    <h1>Hello World</h1>
+  </div>
+);
+
+}
+
+export default App;
